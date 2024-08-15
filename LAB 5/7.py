cfcaa3d013a4c0966b91afe3469964f7082d79aa
Devesh Tulshyan <60965358+Devesh225@users.noreply.github.com>
2024-08-15 20:04:17 +0530

    Update 7.py

--- LAB 5/7.py
+++ LAB 5/7.py
@@ -1,3 +1,16 @@
+'''
+Create a student dictionary and add first_name, last_name, gender, age, marital status, skills,
+country, city and address as keys for the dictionary
+I. Get the length of the student dictionary
+II. Get the value of skills and check the data type, it should be a list
+III. Modify the skills values by adding one or two skills
+IV. Get the dictionary keys as a list
+V. Get the dictionary values as a list
+VI. Change the dictionary to a list of tuples using _items()_ method
+VII. Delete one of the items in the dictionary
+VIII. Delete one of the dictionaries
+'''
+
 student = {
     'first_name': 'John',
     'last_name': 'Doe',
