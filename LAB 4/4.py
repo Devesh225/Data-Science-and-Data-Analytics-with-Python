553aaadbc6df7e5d895e5cd294443aed1e1c3b90
Devesh Tulshyan <60965358+Devesh225@users.noreply.github.com>
2024-08-15 19:57:57 +0530

    Update 4.py

--- LAB 4/4.py
+++ LAB 4/4.py
@@ -1,3 +1,6 @@
+# Write a program that accepts a sequence of whitespace separated words as input and prints the
+# words after removing all duplicate words and sorting them alphanumerically.
+
 s = input("Enter a sequence of whitespace separated words: ")
 words = s.split()
 unique = list(set(words))
