ef3feabf1d7b66bb88413f5a1a8f59df12ee8420
Devesh Tulshyan <60965358+Devesh225@users.noreply.github.com>
2024-08-15 19:59:04 +0530

    Update 6.py

--- LAB 4/6.py
+++ LAB 4/6.py
@@ -1,3 +1,6 @@
+# Write a program which accepts a string as input to print "Yes" if the string is "yes" or "YES" or
+# "Yes", otherwise print "No".
+
 s = input("Enter a string: ")
 if s == "yes" or s == "YES" or s == "Yes":
     print("Yes")
