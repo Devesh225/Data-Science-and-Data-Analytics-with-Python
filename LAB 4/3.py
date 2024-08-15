1cff71ea16e72cb223be7e53eb630b0801676d0c
Devesh Tulshyan <60965358+Devesh225@users.noreply.github.com>
2024-08-15 19:57:39 +0530

    Update 3.py

--- LAB 4/3.py
+++ LAB 4/3.py
@@ -1,3 +1,6 @@
+# Write a program that accepts sequence of lines as input and prints the lines after making all
+# characters in the sentence capitalized.
+
 lines = []
 print("Enter a sequence of lines (press Enter on an empty line to finish):")
 while True:
