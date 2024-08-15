122a7d54c260eef7d27184809b36dd5c582a0ac2
Devesh Tulshyan <60965358+Devesh225@users.noreply.github.com>
2024-08-15 19:57:25 +0530

    Update 2.py

--- LAB 4/2.py
+++ LAB 4/2.py
@@ -1,3 +1,6 @@
+# Write a program that accepts a comma separated sequence of words as input and prints the words
+# in a comma-separated sequence after sorting them alphabetically.
+
 s = input("Enter a comma separated sequence of words: ")
 words = s.split(',')
 words = [word.strip() for word in words]
