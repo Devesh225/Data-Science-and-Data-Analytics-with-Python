b651d32ad839aae802a1af10f4677837e2545a2b
Devesh Tulshyan <60965358+Devesh225@users.noreply.github.com>
2024-08-15 20:01:14 +0530

    Update 1.py

--- LAB 5/1.py
+++ LAB 5/1.py
@@ -1,3 +1,14 @@
+'''
+1.The following is a list of 10 students ages:
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+I. Sort the list and find the min and max age
+II. Add the min age and the max age again to the list
+III. Find the median age (one middle item or two middle items divided by two)
+IV. Find the average age (sum of all items divided by their number )
+V. Find the range of the ages (max minus min)
+VI. Compare the value of (min - average) and (max - average), use _abs()_ method
+'''
+
 ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 ages.sort()
 min_age = ages[0]
