440afa1362cc3587677d4c7fb6f01cadf46872c9
Devesh Tulshyan <60965358+Devesh225@users.noreply.github.com>
2024-08-15 19:59:36 +0530

    Update 8.py

--- LAB 4/8.py
+++ LAB 4/8.py
@@ -1,3 +1,5 @@
+# Write a program which count and print the numbers of each character in a string input by console.
+
 from collections import Counter
 s = input("Enter a string: ")
 characterCount = Counter(s)
