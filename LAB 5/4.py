1ed8f4caea90c2007593b86d4443bc37ea763ca9
Devesh Tulshyan <60965358+Devesh225@users.noreply.github.com>
2024-08-15 20:03:24 +0530

    Update 4.py

--- LAB 5/4.py
+++ LAB 5/4.py
@@ -1,3 +1,16 @@
+'''
+Create a set given below
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+I. Find the length of the set it_companies
+II. Add &#39;Twitter&#39; to it_companies
+III. Insert multiple IT companies at once to the set it_companies
+IV. Remove one of the companies from the set it_companies
+V. What is the difference between remove and discard
+'''
+
 it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
 A = {19, 22, 24, 20, 25, 26}
 B = {19, 22, 20, 25, 26, 24, 28, 27}
